a43cbc0cc668905b6a6487c1fc6637bb89c7fc7a
ISRAEL TEJEDA <rl106765@rlas-macair-15.local>
2018-12-17 11:45:30 -0600

    Initial Commit

---
+++ CameraDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CameraDemo
+//
+//  Created by ISRAEL TEJEDA on 12/4/18.
+//  Copyright © 2018 ISRAEL TEJEDA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
